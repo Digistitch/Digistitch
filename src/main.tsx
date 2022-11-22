01aab5c5374451ac24b6af135273933957534339
Wilson Ibekason <wanbekgithub@gmail.com>
2022-11-22 19:37:10 +0100

    Add Fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/source/main.less";
+import { BrowserRouter as Router } from "react-router-dom";
 import ScrollToTop from "./utils/scrollTotop";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ScrollToTop>
-      <App />
-    </ScrollToTop>
+    <Router>
+      <ScrollToTop>
+        <App />
+      </ScrollToTop>
+    </Router>
   </React.StrictMode>
 );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Blue: "#007bff",
+        Indigo: "#6610f2",
+        Purple: "#6f42c1",
+        Pink: "#e83e8c",
+        Red: "#dc3545",
+        Orange: "#fd7e14",
+        Yellow: "#ffc107",
+        Green: "#28a745",
+        Teal: "#20c997",
+        Cyan: "#17a2b8",
+        White: "#fff",
+        Gray: "#6c757d",
+        Gray_dark: "#343a40",
+        Primary: "#007bff",
+        Secondary: "#6c757d",
+        Success: "#28a745",
+        Info: "#17a2b8",
+        Warning: "#ffc107",
+        Danger: "#dc3545",
+        Light: "#f8f9fa",
+        Dark: "#343a40",
+      },
+      screens: {
+        breakpoin_xs: " 0",
+        breakpoin_sm: "576px",
+        breakpoin_md: "768px",
+        breakpoin_lg: "992px",
+        breakpoin_xl: "1200px",
+      },
+      fontFamily: {
+        flaticon: "Flaticon",
+      },
+    },
   },
   plugins: [],
 };
